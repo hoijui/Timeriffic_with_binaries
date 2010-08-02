e50d3f8ae01b854105ec59c0b262655e6cbd597e
RDRR Labs <rdrr.labs@gmail.com>
2010-08-02 02:32:59 +0000

    Initial skeleton.

---
+++ TimerifficUnitTests/tests/com/alfray/timeriffic/storage/ActionTest.java
@@ -0,0 +1,49 @@
+/*
+ * (c) ralfoide gmail com, 2008
+ * Project: TimerifficUnitTests
+ * License TBD
+ */
+
+package com.alfray.timeriffic.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//-----------------------------------------------
+
+public class ActionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testAction() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetHourMin() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDays() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetActions() {
+        fail("Not yet implemented");
+    }
+
+}
+
+
